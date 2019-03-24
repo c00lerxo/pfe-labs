91adaf6156630ca72ff121cc797713daa0309263
c00lerxo <szymonkuleczka98@gmail.com>
2019-03-24 22:57:27 +0100

    Finish task 6

--- labs2/task6.py
+++ labs2/task6.py
@@ -1,15 +1,13 @@
 import matplotlib.pyplot as plt
-import numpy as np
-from matplotlib import cm
 
 with open('data3.dat') as f:
-  data = f.readlines()
+    data = f.readlines()
 
 x = [float(i) for line in data for i in line.split(',')]
 
 fig = plt.figure()
-ax1 = fig.add_subplot(121)
-bp1 = ax1.boxplot(x)
-
+ax = fig.add_subplot(111)
+ax.set_aspect(16/9)  # Not sure about this one...
+bp = ax.boxplot(x)
 
 plt.show()
