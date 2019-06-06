1f3d12f1d774c7ff55e80f24c363d5303ac7fce3
Szymon Kuleczka <s434596@pc01l.labs.wmi.amu.edu.pl>
2019-06-06 08:49:21 +0200

    finish task 7.6

--- labs7/task6.py
+++ labs7/task6.py
@@ -2,8 +2,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats as stats
 
-with np.load('rozklady.npz') as data:
-    arr_0 = data['arr_0']
-    arr_1 = data['arr_1']
-    arr_2 = data['arr_2']
+with np.load('labs7/rozklady.npz') as data:
+    pairs = (data['arr_0'], data['arr_1'], data['arr_2'])
 
+x = np.linspace(-6, 6, num=100)
+fig = plt.figure()
+
+for index, pair in enumerate(pairs, start=1):
+    print('Pair', index)
+    pair_densities = (stats.gaussian_kde(pair[0]), stats.gaussian_kde(pair[1]))
+
+    means = (np.mean(pair[0]), np.mean(pair[1]))
+    print('Means: ', means[0], means[1])
+    
+    print('Null-hypothesis test:')
+    print(stats.ttest_1samp(pair[0], 0.0))
+    print(stats.ttest_1samp(pair[1], 0.0))
+
+    print('Means test:')
+    print(stats.ttest_ind(pair[0], pair[1]))
+
+    ax = fig.add_subplot(1, 3, index)
+    ax.plot(x, pair_densities[0](x), color='blue')
+    ax.plot(x, pair_densities[1](x), color='green')
+
+plt.show()
